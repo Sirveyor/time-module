6faa83c6ba30be3ad977ddd3b31370890bb09a01
Covert Nelson <sirveyor@debsnest.com>
2024-04-01 08:13:18 -0400

    sqlalchemy testing

--- dbtest.py
+++ dbtest.py
@@ -1,4 +1,10 @@
 # dbtest.py
 
 # Connecting
-from sq
+from sqlalchemy import create_engine, text
+
+engine = create_engine("sqlite:///./data/books.db", echo=True, future=True)
+conn = engine.connect()
+result = conn.execute(text("select * from author"))
+print(result.all())
+conn.close()
